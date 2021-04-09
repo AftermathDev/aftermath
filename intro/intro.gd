e3eef98ce95051a9404013f767f7ee519541d89d
ollintlaloc@gmail.com <46585763+rullinoiz@users.noreply.github.com>
2021-04-09 09:51:52 -0700

    better intro

--- intro/intro.gd
+++ intro/intro.gd
@@ -1,3 +1,6 @@
+# le epic main script
+# think of this as a server script in roblox
+
 extends Node2D
 
 # get all important nodes
@@ -5,12 +8,16 @@ onready var camera = get_node("Camera2D")
 onready var loopmusic = get_node("loopmusic")
 onready var music = get_node("intromusic")
 onready var background = get_node("background")
+onready var introintro = get_node("introintromusic")
 
 func _ready(): # fires when game starts
 	camera.current = true # make the player see through camera2D
 	main()
 
 func main():
+	introintro.play(0)
 	background.play("stars")
 	yield(get_tree().create_timer(1.0), "timeout") # https://godotengine.org/qa/7042/wait-like-function
-	music.play(0)
+	camera.get_node("AnimationPlayer").play("slide to the left")
+	
+	
